423e42136a854d8c75b7ebf019eb9db4f588faa4
Aziz Amerul Faozi <cahalapisan@gmail.com>
2017-11-11 11:10:07 +0700

    Aziz Keren

---
+++ resource/Starting with GHDL — GHDL 2017-03-01 documentation_files/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "ghdl",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://ghdl.readthedocs.io/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/doc/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "9a90393e"
+  };
+  
